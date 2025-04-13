8f33df03e90b73e02ecc9934b781c57af2fc11f3
meo209 <redslyice@outlook.de>
2025-04-14 01:26:10 +0200

    build: Publish to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    `maven-publish`
+    signing
 }
 
 group = "io.github.0x1bd"
@@ -19,4 +21,65 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            artifactId = "ksignal"
+            from(components["java"])
+
+            pom {
+                name.set("KSignal")
+                description.set(project.description)
+                url.set("https://github.com/0x1bd/KSignal")
+
+                licenses {
+                    license {
+                        name.set("GNU GPLv3")
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("0x1bd")
+                        name.set("kvxd")
+                        email.set("0x1bd@proton.me")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/0x1bd/KSignal.git")
+                    developerConnection.set("scm:git:ssh://github.com/0x1bd/KSignal.git")
+                    url.set("https://github.com/0x1bd/KSignal")
+                }
+            }
+
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
